f817c12319eb07643540b0d36cbfd48f7870b596
Aimar <aimar@snapit.ee>
2018-05-13 21:57:07 +0200

    added title

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to DevOps to AWS!</title>
+  </head>
+  <body>
+    <?php
+      require 'Welcome.php';
+      $welcome = new Welcome();
+    ?>
+    <h1><?php echo $welcome->greet() ?></h1>
+  </body>
+</html>
